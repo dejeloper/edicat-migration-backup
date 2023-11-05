ad2308d0ed4fec236999d8e830a718d0a5d8f4ff
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-11-04 19:40:16 -0500

    Generar Menu base

--- app.js
+++ app.js
@@ -7,98 +7,126 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const connectionSucursal = mysql.createConnection({
-  host: "edicat.com.co",
-  user: "edicat_Administrador",
-  password: "Administrador01*",
-  database: "edicat_jsucursal",
-});
-
-const connectionPlataforma = mysql.createConnection({
-  host: "edicat.com.co",
-  user: "edicat_Administrador",
-  password: "Administrador01*",
-  database: "edicat_Plataforma",
-});
-
-rl.question("Ingresa el ID de la consulta: ", (id) => {
-  conexionSucursal(id);
-
-  rl.close();
-});
-
-const conexionSucursal = (id) => {
-  connectionSucursal.connect((err) => {
-    if (err) {
-      console.error("Error de conexión a la base de datos: " + err.message);
-      return;
+function mostrarMenu() {
+  console.log("Bienvenido a la migración de su base de datos:");
+  console.log("1. Módulo Master");
+  console.log("2. Módulo Negocio");
+  console.log("3. Módulo Administrativo");
+  console.log("4. Módulo Autogenerables");
+  console.log("5. Salir");
+  rl.question("Seleccione una opción: ", (opcion) => {
+    if (opcion === "1") {
+      console.log("Seleccionaste la Opción 1");
+    } else if (opcion === "2") {
+      console.log("Seleccionaste la Opción 2");
+    } else if (opcion === "3") {
+      console.log("Seleccionaste la Opción 3");
+    } else if (opcion === "4") {
+      console.log("Seleccionaste la Opción 4");
+    } else if (opcion === "5") {
+      console.log("Saliendo del programa");
+      rl.close();
+    } else {
+      console.log("Opción no válida. Por favor, elige una opción válida.");
+      mostrarMenu();
     }
-    console.log("Conexión a la base de datos MySQL establecida.");
   });
+}
 
-  const query = `SELECT * FROM edicat_jsucursal.Usuarios limit ${id}`;
+mostrarMenu();
 
-  connectionSucursal.query(`${query}`, (err, results) => {
-    if (err) {
-      console.error("Error en la consulta: " + err.message);
-      return;
-    }
+// const connectionSucursal = mysql.createConnection({
+//   host: "edicat.com.co",
+//   user: "edicat_Administrador",
+//   password: "Administrador01*",
+//   database: "edicat_jsucursal",
+// });
 
-    results.forEach((element) => {
-      const {
-        Codigo,
-        Usuario,
-        Pass,
-        Salt,
-        Nombre,
-        TipoDocumento,
-        Documento,
-        Perfil,
-        Administrador,
-        Estado,
-        CambioPass,
-        Habilitado,
-        UsuarioCreacion,
-        FechaCreacion,
-        UsuarioModificacion,
-        FechaModificacion,
-      } = element;
+// const connectionPlataforma = mysql.createConnection({
+//   host: "edicat.com.co",
+//   user: "edicat_Administrador",
+//   password: "Administrador01*",
+//   database: "edicat_Plataforma",
+// });
 
-      const FechaCreacionString =
-        FechaCreacion !== null
-          ? FechaCreacion.toISOString().slice(0, 19).replace("T", " ")
-          : null;
+// rl.question("Ingresa el ID de la consulta: ", (id) => {
+//   conexionSucursal(id);
 
-      const FechaModificacionString =
-        FechaModificacion !== null
-          ? FechaModificacion.toISOString().slice(0, 19).replace("T", " ")
-          : null;
+//   rl.close();
+// });
 
-      // console.log("FechaCreacion", FechaCreacion, FechaCreacionString);
-      // console.log(
-      //   "FechaModificacion",
-      //   FechaModificacion,
-      //   FechaModificacionString
-      // );
-      // console.log(" ");
+// const conexionSucursal = (id) => {
+//   connectionSucursal.connect((err) => {
+//     if (err) {
+//       console.error("Error de conexión a la base de datos: " + err.message);
+//       return;
+//     }
+//     console.log("Conexión a la base de datos MySQL establecida.");
+//   });
 
-      const query = `INSERT INTO edicat_Plataforma.Usuarios (Usuario, Pass, Salt, Nombre, TipoDocumento, Documento, Perfil, Administrador, Estado, CambioPass, Habilitado, UsuarioCreacion, FechaCreacion, UsuarioModificacion, FechaModificacion) VALUES ('${Usuario}', '${Pass}', '${Salt}', '${Nombre}', '${TipoDocumento}', '${Documento}', '${Perfil}', '${Administrador}', '${Estado}', '${CambioPass}', '${Habilitado}', '${UsuarioCreacion}', '${FechaCreacionString}', '${UsuarioModificacion}', '${FechaModificacionString}');`;
+//   const query = `SELECT * FROM edicat_jsucursal.Usuarios limit ${id}`;
 
-      console.log(query);
+//   connectionSucursal.query(`${query}`, (err, results) => {
+//     if (err) {
+//       console.error("Error en la consulta: " + err.message);
+//       return;
+//     }
 
-      // create a file stream and pass it the query and overwrite the file if it exists
-      const file = fs.createWriteStream("query.sql", { flags: "a" });
-      file.write(`${query}\n\n\n`);
-      file.end();
-    });
+//     results.forEach((element) => {
+//       const {
+//         Codigo,
+//         Usuario,
+//         Pass,
+//         Salt,
+//         Nombre,
+//         TipoDocumento,
+//         Documento,
+//         Perfil,
+//         Administrador,
+//         Estado,
+//         CambioPass,
+//         Habilitado,
+//         UsuarioCreacion,
+//         FechaCreacion,
+//         UsuarioModificacion,
+//         FechaModificacion,
+//       } = element;
 
-    //console.log("Resultados de la consulta:", results);
-  });
+//       const FechaCreacionString =
+//         FechaCreacion !== null
+//           ? FechaCreacion.toISOString().slice(0, 19).replace("T", " ")
+//           : null;
 
-  connectionSucursal.end((err) => {
-    if (err) {
-      console.error("Error al cerrar la conexión: " + err.message);
-    }
-    console.log("Conexión a la base de datos MySQL cerrada.");
-  });
-};
+//       const FechaModificacionString =
+//         FechaModificacion !== null
+//           ? FechaModificacion.toISOString().slice(0, 19).replace("T", " ")
+//           : null;
+
+//       // console.log("FechaCreacion", FechaCreacion, FechaCreacionString);
+//       // console.log(
+//       //   "FechaModificacion",
+//       //   FechaModificacion,
+//       //   FechaModificacionString
+//       // );
+//       // console.log(" ");
+
+//       const query = `INSERT INTO edicat_Plataforma.Usuarios (Usuario, Pass, Salt, Nombre, TipoDocumento, Documento, Perfil, Administrador, Estado, CambioPass, Habilitado, UsuarioCreacion, FechaCreacion, UsuarioModificacion, FechaModificacion) VALUES ('${Usuario}', '${Pass}', '${Salt}', '${Nombre}', '${TipoDocumento}', '${Documento}', '${Perfil}', '${Administrador}', '${Estado}', '${CambioPass}', '${Habilitado}', '${UsuarioCreacion}', '${FechaCreacionString}', '${UsuarioModificacion}', '${FechaModificacionString}');`;
+
+//       console.log(query);
+
+//       // create a file stream and pass it the query and overwrite the file if it exists
+//       const file = fs.createWriteStream("query.sql", { flags: "a" });
+//       file.write(`${query}\n\n\n`);
+//       file.end();
+//     });
+
+//     //console.log("Resultados de la consulta:", results);
+//   });
+
+//   connectionSucursal.end((err) => {
+//     if (err) {
+//       console.error("Error al cerrar la conexión: " + err.message);
+//     }
+//     console.log("Conexión a la base de datos MySQL cerrada.");
+//   });
+// };
